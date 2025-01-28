61bf179ccf4e3fca3d4921c65dde8f66395911f0
NPHH <86760286+NPHH@users.noreply.github.com>
2025-01-29 01:00:41 +0700

    eqweq

---
+++ ESP32/ESP32 Test/ESP32 Test/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+
+void setup() 
+{
+    Serial.begin(115200);
+
+    Serial.println("Begin...");
+
+
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+}
